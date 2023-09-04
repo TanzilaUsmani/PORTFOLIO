626aebe8bacf03262b8c37852ce3c65622c180d0
TanzilaUsmani <tanzila.usmani.tanzu10@gmail.com>
2023-09-05 01:24:24 +0530

    Initial commit msg

---
+++ message.js
@@ -0,0 +1,15 @@
+function london(){
+    document.getElementById("London").style.display="block";
+    document.getElementById("Paris").style.display="none";
+    document.getElementById("Tokyo").style.display="none";
+}
+function paris(){
+document.getElementById("Paris").style.display="block"
+document.getElementById("London").style.display="none"
+document.getElementById("Tokyo").style.display="none"
+}
+function tokyo(){
+document.getElementById("Tokyo").style.display="block"
+document.getElementById("Paris").style.display="none"
+document.getElementById("London").style.display="none"
+}
